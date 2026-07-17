7d7a171464f828bcf28d5f1d1ccda47ec771100f
Mirek Rusin <mirek@users.noreply.github.com>
2026-07-17 17:19:53 +0200

    correct descending SemVer precedence tests

--- packages/semver/src/string-dsc.test.ts
+++ packages/semver/src/string-dsc.test.ts
@@ -2,7 +2,7 @@ import * as Semver from './index.js'
 import { test } from 'node:test'
 import assert from 'node:assert/strict'
 
-await test('simple', () => {
+await test('sorts stable releases ahead of prereleases in descending order', () => {
   assert.deepEqual([
     '1.0.0-alpha.beta',
     '1.0.0',
@@ -16,10 +16,25 @@ await test('simple', () => {
     'v2.0.1',
     'v2.0.0',
     '1.1.0',
+    '1.0.0',
     '1.0.0-beta',
     '1.0.0-alpha.beta',
     '1.0.0-alpha.1',
-    '1.0.0-alpha',
-    '1.0.0'
+    '1.0.0-alpha'
   ])
+})
+
+await test('is the exact reverse of ascending precedence', () => {
+  const versions = [
+    '1.0.0-alpha',
+    '1.0.0-alpha.1',
+    '1.0.0-alpha.beta',
+    '1.0.0-beta',
+    '1.0.0-beta.2',
+    '1.0.0-beta.11',
+    '1.0.0-rc.1',
+    '1.0.0'
+  ]
+
+  assert.deepEqual(versions.toSorted(Semver.stringDsc), versions.toReversed())
 })
