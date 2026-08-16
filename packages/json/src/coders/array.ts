9fe3f6eb72439ce57ef58fd0598261d1cfe6c5f9
Mirek Rusin <mirek@users.noreply.github.com>
2026-08-16 15:27:53 +0200

    Detect cycles through Map, Set and Error values in the JSON encoder (#162)

--- packages/json/src/coders/array.ts
+++ packages/json/src/coders/array.ts
@@ -1,6 +1,5 @@
 import * as Encoder from '../encoder.js'
 import * as Decoder from '../decoder.js'
-import { ancestors } from './object.js'
 
 export type t = unknown[]
 export const constructor = Array
@@ -8,28 +7,20 @@ export const name = 'Array'
 
 export const encode =
   (input: t, encoder: Encoder.t) => {
-    if (ancestors.has(input)) {
-      throw new TypeError('Converting circular structure to JSON.')
-    }
-    ancestors.add(input)
-    try {
-      let output: unknown[] = input
-      const n = input.length
-      for (let i = 0; i < n; i++) {
-        const inputValue = input[i]
-        const value = Encoder.encode(inputValue, encoder)
-        if (inputValue === value) {
-          continue
-        }
-        if (output === input) {
-          output = input.slice()
-        }
-        output[i] = value
+    let output: unknown[] = input
+    const n = input.length
+    for (let i = 0; i < n; i++) {
+      const inputValue = input[i]
+      const value = Encoder.encode(inputValue, encoder)
+      if (inputValue === value) {
+        continue
       }
-      return output
-    } finally {
-      ancestors.delete(input)
+      if (output === input) {
+        output = input.slice()
+      }
+      output[i] = value
     }
+    return output
   }
 
 export const decode =
